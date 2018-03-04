20d140590cdd681e4e5bab7d13c51aa8d5d3a06a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-03-04 20:43:31 +0900

    writing skelton

---
+++ build_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GITBUCKET_VERSION=4.20.0
+
+pwd
+
+echo $HOME
+
+for target in plugins/*.sh; do
+    echo $target
+    . $target
+    mkdir $PLUGIN_NAME
+    
+    pushd $PLUGIN_NAME
+    
+    wget $PLUGIN_TGZ_URL
+    tar zxvf $(basename $PLUGIN_TGZ_URL)
+    cd $PLUGIN_SRC_DIR
+
+    sed -i -e "s/gitbucketVersion *:= *\"[0-9.]*\"/gitbucketVersion := \"${GITBUCKET_VERSION}\"/" build.sbt
+    sbt assembly
+
+    ls target/scala-2.12
+
+    popd
+done
+
