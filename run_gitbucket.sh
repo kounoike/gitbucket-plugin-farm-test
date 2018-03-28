a1ea1bea532e2557d5e65503f8d36a804ae38ed2
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-03-28 16:24:09 +0900

    Oops doesn't extracted

--- run_gitbucket.sh
+++ run_gitbucket.sh
@@ -3,9 +3,11 @@
 ### build gitbucket-master because plugin list API doesn't released yet
 
 GITBUCKET_TGZ_URL=https://github.com/gitbucket/gitbucket/archive/master.tar.gz
-GITBUCKET_SRC_DIR=gitbucket-master
+GITBUCKET_SRC_DIR=master
 
 wget $GITBUCKET_TGZ_URL
+
+tar zxf $(basename $GITBUCKET_TGZ_URL)
 
 pushd $GITBUCKET_SRC_DIR
 
