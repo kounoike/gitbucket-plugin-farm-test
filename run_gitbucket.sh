3b2c8ab4bb299c78604774209a7a61acb47593f0
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-08 14:08:37 +0900

    Oops pushd removed but popd exists

--- run_gitbucket.sh
+++ run_gitbucket.sh
@@ -3,11 +3,9 @@
 API_URL=https://api.github.com/repos/gitbucket/gitbucket/releases/latest
 GITBUCKET_URL=$( curl -s $API_URL | jq -r '.assets[] | select(.name == "gitbucket.war") | .url ')
 
-wget -O gitbucket.war $GITBUCKET_URL
+wget -O $HOME/gitbucket.war $GITBUCKET_URL
 
-nohup java -jar gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
+nohup java -jar $HOME/gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
 
 sleep 10
 cat $HOME/gitbucket.log
-
-popd
