2fd0aa21340b008ae482e5342499cebda99dfb73
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-08 13:55:41 +0900

    fix download url

--- run_gitbucket.sh
+++ run_gitbucket.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-GITBUCKET_URL=https://github.com/gitbucket/gitbucket/releases/download/latest/gitbucket.war
+GITBUCKET_URL=$( curl -s https://api.github.com/repos/gitbucket/gitbucket/releases/latest 
+  | jq -r '.assets[] | select(.name == "gitbucket.war") | .url ')
 
-wget $GITBUCKET_URL
+wget -O gitbucket.war $GITBUCKET_URL
 
 nohup java -jar gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
 
