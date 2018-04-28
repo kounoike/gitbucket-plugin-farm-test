bca72c1c112106a4782bb04bf65d111f9a0a6411
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-29 04:12:45 +0900

    fix test.sh

--- plugins/explorer/test.sh
+++ plugins/explorer/test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl http://localhost:8080/root/explorer-repo/explore > &2
+wget -O explore.json http://localhost:8080/root/explorer-repo/explore || exit 1
+grep -q test.txt explore.json || exit 1
+grep -q file.txt explore.json || exit 1
+
+exit 0
