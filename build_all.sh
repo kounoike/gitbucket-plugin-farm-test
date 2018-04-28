7de7a19c9d0375d256c0b32ad1f748a8c0794aa8
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-29 03:59:42 +0900

    put plugin list output to null

--- build_all.sh
+++ build_all.sh
@@ -45,7 +45,7 @@ buildPlugin() {
     # check plugin list api
     sleep 5 # wait for load plugin
     plugins=$(curl -sS http://localhost:8080/api/v3/gitbucket/plugins)
-    echo $plugins | jq -e ".[] | select(.id == \"${PLUGIN_ID}\")" || return
+    echo $plugins | jq -e ".[] | select(.id == \"${PLUGIN_ID}\")" > /dev/null || return
 
     # create repository for test
     if [ -d ${target}-repo ]; then
