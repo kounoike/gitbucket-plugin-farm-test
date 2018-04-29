f4958321c61e47ba7e9d5247d08fd0f42c291ea2
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-29 10:50:25 +0900

    Oops, wget gitbucket.war location changed

--- run_gitbucket.sh
+++ run_gitbucket.sh
@@ -5,7 +5,7 @@
 GITBUCKET_URL=https://github.com/gitbucket/gitbucket/releases/download/${GITBUCKET_VERSION}/gitbucket.war
 
 if [ ! -e $HOME/Downloads/gitbucket.war ]; then
-    wget -q -O $HOME/gitbucket.war $GITBUCKET_URL
+    wget -q -O $HOME/Downloads/gitbucket.war $GITBUCKET_URL
 fi
 
 nohup java -jar $HOME/Downloads/gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
