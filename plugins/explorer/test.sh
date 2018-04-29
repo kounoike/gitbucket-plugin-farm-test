c1e920ccdf53a5ac17b6406a584391c8fb003b6a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-29 11:42:04 +0900

    Oops, dir/file.txt doesn't show in owner/repo/explore

--- plugins/explorer/test.sh
+++ plugins/explorer/test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 wget -O explore.json http://localhost:8080/root/explorer-repo/explore || exit 1
-#debug
-cat explore.json
 
 grep -q test.txt explore.json || exit 1
-grep -q file.txt explore.json || exit 1
 
 exit 0
