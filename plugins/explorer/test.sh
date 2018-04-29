276f7d968a0baff136f6552858f001698733c88a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-29 11:08:18 +0900

    debug explorer/test.sh

--- plugins/explorer/test.sh
+++ plugins/explorer/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 wget -O explore.json http://localhost:8080/root/explorer-repo/explore || exit 1
+#debug
+cat explore.json
+
 grep -q test.txt explore.json || exit 1
 grep -q file.txt explore.json || exit 1
 
