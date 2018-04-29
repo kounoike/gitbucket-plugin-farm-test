c99f2171fae4cb9eda44e525e95f1f98c26c4478
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-29 11:54:32 +0900

    grep check

--- run_gitbucket.sh
+++ run_gitbucket.sh
@@ -10,4 +10,6 @@ fi
 
 nohup java -jar $HOME/Downloads/gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
 
-tail -f $HOME/gitbucket.log | grep -m 1 --line-buffered "Started ServerConnector"
+grep --help
+
+tail -f $HOME/gitbucket.log | grep --max-count=1 --line-buffered "Started ServerConnector"
