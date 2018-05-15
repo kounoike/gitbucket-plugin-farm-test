74c32051656b10c93cf36c1db1e8d4f87a34e49a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-05-16 03:43:27 +0900

    use GitbucketVersion on build.sbt @master

--- gitbucket_version.sh
+++ gitbucket_version.sh
@@ -2,9 +2,19 @@ GITBUCKET_VERSION=4.23.1
 SCALA_VERSION=2.12.4
 SBT_VERSION=1.1.1
 
-#if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
-if true; then
+if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
     GITBUCKET_USE_MASTER=yes
+    GITBUCKET_TGZ_URL=https://github.com/gitbucket/gitbucket/archive/master.tar.gz
+    GITBUCKET_SRC_DIR=$(pwd)/gitbucket-master
+
+    if [ ! -e $HOME/master.tar.gz ]; then
+        wget -O $HOME/master.tar.gz $GITBUCKET_TGZ_URL
+        tar zxf $HOME/master.tar.gz
+    fi
+
+    pushd $GITBUCKET_SRC_DIR
+         GITBUCKET_VERSION=$(sed -ne 's/val GitBucketVersion *= *"\(.*\)"/\1/p' build.sbt)
+    popd
 else
     GITBUCKET_USE_MASTER=no
 fi
--- run_gitbucket.sh
+++ run_gitbucket.sh
@@ -5,24 +5,16 @@
 echo EVENT_TYPE:${TRAVIS_EVENT_TYPE}
 
 if [ "$GITBUCKET_USE_MASTER" = "yes" ]; then
-    GITBUCKET_TGZ_URL=https://github.com/gitbucket/gitbucket/archive/master.tar.gz
-    GITBUCKET_SRC_DIR=gitbucket-master
-
-    wget $GITBUCKET_TGZ_URL
-
-    tar zxf $(basename $GITBUCKET_TGZ_URL)
-
     pushd $GITBUCKET_SRC_DIR
-
     sbt executable publishLocal
     nohup java -jar target/executable/gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
+    popd
 else
     GITBUCKET_URL=https://github.com/gitbucket/gitbucket/releases/download/${GITBUCKET_VERSION}/gitbucket.war
 
     if [ ! -e $HOME/Downloads/gitbucket.war ]; then
         wget -q -O $HOME/Downloads/gitbucket.war $GITBUCKET_URL
     fi
-
     nohup java -jar $HOME/Downloads/gitbucket.war >$HOME/gitbucket.log 2>&1 < /dev/null &
 fi
 
